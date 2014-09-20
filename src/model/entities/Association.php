2f3ba0c43bebf3b62894b6f9b783f1682e8722d6
Rasmus Eneman <rasmus@eneman.eu>
2014-09-20 14:54:29 +0200

    Add Association tests

---
+++ src/model/entities/Association.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace model\entities;
+
+class Association {
+
+    /**
+     * @param string $string
+     */
+    public function __construct($string) {
+
+    }
+
+    /**
+     * @returns string
+     */
+    public function getName() {
+
+    }
+
+    /**
+     * @returns string
+     */
+    public function getFrom() {
+
+    }
+
+    /**
+     * @returns string
+     */
+    public function getTo() {
+
+    }
+}
---
+++ test/model/entities/AssociationTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace model\entities;
+
+require_once('src/model/entities/Association.php');
+
+class AssociationTest extends \PHPUnit_Framework_TestCase {
+
+    public function testThatItParsesAssociationsInGarbage() {
+        $association = new Association('dgfshdfgh[Member]-[Comment]gfdfgsgsdfg');
+
+        $this->assertNull($association->getName());
+        $this->assertEquals('Member', $association->getFrom());
+        $this->assertEquals('Comment', $association->getTo());
+    }
+
+    public function testThatItParsesAssociationsWithName() {
+        $association = new Association('[Member]-Posts-[Comment]');
+
+        $this->assertEquals('Posts', $association->getName());
+        $this->assertEquals('Member', $association->getFrom());
+        $this->assertEquals('Comment', $association->getTo());
+    }
+}
