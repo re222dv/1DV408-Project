e27532345508bdddf16d6b368b8b49e71efcc444
Rasmus Eneman <rasmus@eneman.eu>
2014-10-13 18:30:53 +0200

    style: Split positioning logic into multiple functions

--- src/view/diagrams/ClassDiagramView.php
+++ src/view/diagrams/ClassDiagramView.php
@@ -9,6 +9,9 @@ use view\entities\ClassObjectView;
 
 class ClassDiagramView extends View {
     protected $template = 'diagrams/classDiagram.svg';
+    /**
+     * @var ClassDiagram
+     */
     private $classDiagram;
 
     public function setDiagram(ClassDiagram $classDiagram) {
@@ -26,11 +29,32 @@ class ClassDiagramView extends View {
             $classes[$class->getName()] = $view;
         }
 
+        $this->positionClasses($classes);
 
+        foreach ($classDiagram->getAssociations() as $association) {
+            $from = $classes[$association->getFrom()];
+            $to = $classes[$association->getTo()];
+
+            $view = new AssociationView($this->settings);
+            $view->setAssociation($association, $from, $to);
+            $this->variables['associations'][] = $view;
+        }
+    }
+
+    private function positionClasses($classes) {
+        $positions = [];
+
+        $this->positionClassesBelowAssociativeClasses($classes);
+        $this->positionAssociatedClassesNextToEachOther($classes, $positions);
+        $this->positionClassesStraightBelowAssociativeClasses($classes);
+        $this->positionAllClassesNextToEachOther($classes, $positions);
+    }
+
+    private function positionClassesBelowAssociativeClasses($classes) {
         do {
             $modified = false;
 
-            foreach ($classDiagram->getAssociations() as $association) {
+            foreach ($this->classDiagram->getAssociations() as $association) {
                 $from = $classes[$association->getFrom()];
                 $to = $classes[$association->getTo()];
 
@@ -42,46 +66,39 @@ class ClassDiagramView extends View {
                 }
             }
         } while($modified);
+    }
 
-        $positions = [];
-
-        foreach ($classDiagram->getAssociations() as $association) {
+    private function positionAssociatedClassesNextToEachOther($classes, $positions) {
+        foreach ($this->classDiagram->getAssociations() as $association) {
             $from = $classes[$association->getFrom()];
-            $to = $classes[$association->getTo()];
 
             if (isset($positions[$from->top])) {
                 while (isset($positions[$from->top][$from->left])) {
                     $from->left += 1;
                 }
-                $positions[$from->top][$from->left] = $from;
-            } else {
-                $positions[$from->top][$from->left] = $from;
             }
-;
-        }
 
-        foreach ($classDiagram->getAssociations() as $association) {
+            $positions[$from->top][$from->left] = $from;
+        }
+    }
+
+    private function positionClassesStraightBelowAssociativeClasses($classes) {
+        foreach ($this->classDiagram->getAssociations() as $association) {
             $from = $classes[$association->getFrom()];
             $to = $classes[$association->getTo()];
 
             $to->left = $from->left;
         }
+    }
 
+    private function positionAllClassesNextToEachOther($classes, $positions) {
         foreach ($classes as $class) {
             while (isset($positions[$class->top][$class->left]) &&
-                   $positions[$class->top][$class->left] !== $class) {
+                $positions[$class->top][$class->left] !== $class) {
                 $class->left += 1;
             }
+
             $positions[$class->top][$class->left] = $class;
-        }
-
-        foreach ($classDiagram->getAssociations() as $association) {
-            $from = $classes[$association->getFrom()];
-            $to = $classes[$association->getTo()];
-
-            $view = new AssociationView($this->settings);
-            $view->setAssociation($association, $from, $to);
-            $this->variables['associations'][] = $view;
         }
     }
 }
