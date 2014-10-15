ddecf8a99c28b502a9120b02f5ab619871b6ea6a
Rasmus Eneman <rasmus@eneman.eu>
2014-10-15 15:05:33 +0200

    design: Add logo and font

--- src/view/MasterView.php
+++ src/view/MasterView.php
@@ -3,6 +3,7 @@
 namespace view;
 
 use Template\View;
+use view\services\Router;
 
 class MasterView extends View {
     protected $template = 'master.html';
@@ -13,5 +14,9 @@ class MasterView extends View {
 
     public function setMain(View $view) {
         $this->setVariable('main', $view);
+    }
+
+    public function onRender() {
+        $this->variables['indexUrl'] = Router::INDEX;
     }
 }
