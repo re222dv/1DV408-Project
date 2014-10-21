48a9c9eac168b09edde5f6f3323c9dc288a83765
Rasmus Eneman <rasmus@eneman.eu>
2014-10-21 18:10:19 +0200

    fix: Uncaught error handling

---
+++ error.php
@@ -0,0 +1,35 @@
+<?php http_response_code(500); ?>
+<!DOCTYPE html>
+<html>
+    <head lang="en">
+        <meta charset="UTF-8">
+        <title>UMLS</title>
+        <link rel="stylesheet" href="/static/styles/main.css" />
+        <link href='http://fonts.googleapis.com/css?family=Alegreya+Sans+SC:400,300,500,700' rel='stylesheet' type='text/css' />
+        <link href='http://fonts.googleapis.com/css?family=Halant' rel='stylesheet' type='text/css' />
+        <style>
+            main {
+                margin: auto;
+                max-width: 500px;
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+            <section class="logo">
+                <a href="/">
+                    <span class="uml">UML</span>S
+                </a>
+            </section>
+        </header>
+        <main>
+            <h1>An unknown error have occurred</h1>
+            <h2>We are sorry, sincerely</h2>
+            <p>
+                Due to some internal error the page you requested can't be displayed.
+                The error have been logged and will be investigated. If you want
+                you can return to the page and try again or <a href="/">go back to the start</a>.
+            </p>
+        </main>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -8,10 +8,31 @@ use controller\MasterController;
 use Di\Injector;
 use Template\ViewSettings;
 
-$injector = new Injector();
-$injector->bindToInstance(Injector::class, $injector);
-$injector->get(ViewSettings::class)->templatePath = 'src/templates/';
+try {
+    $injector = new Injector();
+    $injector->bindToInstance(Injector::class, $injector);
+    $injector->get(ViewSettings::class)->templatePath = 'src/templates/';
 
-$masterController = $injector->get(MasterController::class);
+    $masterController = $injector->get(MasterController::class);
 
-echo $masterController->render();
+    echo $masterController->render();
+} catch (\Exception $e) {
+    include('error.php');
+
+    $type = get_class($e);
+    $message = $e->getMessage();
+    $trace = $e->getTraceAsString();
+
+    error_log(<<<ERROR
+
+====================================================================================
+Uncaught Exception: $type
+$message
+
+StackTrace:
+$trace
+====================================================================================
+
+ERROR
+    );
+}
